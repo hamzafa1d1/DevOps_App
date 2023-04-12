6dc07ac32e213b5f20b17b9712feb1f3e66104db
hamzafa1d1 <hamza.faidi@supcom.tn>
2023-04-12 22:10:17 +0100

    pushed to branch hamza

--- client/src/views/admin/pipelines/index.jsx
+++ client/src/views/admin/pipelines/index.jsx
@@ -1,24 +1,3 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2022 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 
 // Chakra imports
 import {
@@ -134,7 +113,6 @@ export default function Pipelines() {
       <Banner />
       <Box bg={boxBg} p="4" mt="4">
         <form onSubmit={handleSubmit}>
-
         <FormControl>
             <FormLabel> Description </FormLabel>
                 <Input type="text" name="description" value={formData.description} onChange={handleChange} placeholder="Write your description here!" style={{ width: "100%", height: "100px" }}/>
@@ -273,9 +251,7 @@ export default function Pipelines() {
         )}
       </div>
     </div>
-          
           <Card p ='4' m='10px'>
-
             <FormControl mt ='4'>
               <FormLabel>Unit Testing Framework</FormLabel>
               <Select name="testingFramework" value={formData.testingFramework} onChange={handleChange}>
